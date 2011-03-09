80eaf42586437989bdd6930c4ba7a6f13f0f1e66
digicyc <digicyc@gmail.com>
2011-03-08 22:53:30 -0700

    Started config support. preparing to add the swing calls and setup Roster window and chat window(s)

--- src/main/scala/Dripper.scala
+++ src/main/scala/Dripper.scala
@@ -2,14 +2,22 @@ package org.antitech.dripper
 /*
  * Dripper: Jabber client in Scala using Smack.
  */
+import org.jivesoftware.smack._
+import net.lag.configgy.Configgy
 
 object Dripper {
   def main(args: Array[String]): Unit = {
-    val dripper = new Dripper
-    //dripper.display()
+    // Setup configuration.
+    // TODO: Installer should create folder .dripper and store files there.
+    // TODO: Read conf file from user_home/.dripper/conf
+    val fileSep = System.getProperty("file.separator")
+    val confPath = "conf" + fileSep + "dripper.ini"
+    Configgy.configure(confPath)
+    val config = Configgy.config
+
+    // TODO: Allow multiple connections.
+    val serverAddr = config("server.address")
+    val serverPort = config("server.port")
+    val xmppConfig = new ConnectionConfiguration(serverAddr, serverPort.toInt)
   }
-}
-
-class Dripper {
-
 }
