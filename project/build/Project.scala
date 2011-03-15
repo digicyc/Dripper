03ecc71e7b3f784110c7234898399741ae851f6b
Aaron A <digicyc@gmail.com>
2011-03-15 15:49:46 -0600

    Setting up Spec testing. Added scalatest artifact to project build.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,4 +2,5 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.2"
+  val scalatest = "org.scalatest" % "scalatest" % "1.3"
 }
